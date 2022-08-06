7c09ee7980be1ba5ac1e2d0096d373eafef35c49
Elias Lima <sci.eliaslima@gmail.com>
2022-08-06 10:24:11 -0300

    Apply responsivity to countdown

--- Countdown/assets/scripts.js
+++ Countdown/assets/scripts.js
@@ -1,4 +1,4 @@
-var dateLaunch = new Date('September 11, 2023 23:59:00')
+var dateLaunch = new Date('September 11, 2022 23:59:00')
 var day = document.getElementById("day")
 var hour = document.getElementById("hour")
 var minute = document.getElementById("minute")
