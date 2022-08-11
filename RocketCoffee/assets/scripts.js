2258a0d06caa4067842733f6b1390fcced30e66f
Elias Lima <sci.eliaslima@gmail.com>
2022-08-10 21:18:49 -0300

    Update RocketCoffee page and apply responsivity

--- RocketCoffee/assets/scripts.js
+++ RocketCoffee/assets/scripts.js
@@ -1,18 +1,28 @@
-function openMenu() {
+function closeMenu() {
     
-    const element = document.getElementsByClassName('links')
-    element[0].style.display = 'flex'
-    document.getElementById('menu-button').src = "./assets/menu-buguer-close.svg"
-    document.getElementById('menu-button').setAttribute("onclick","closeMenu()");
+    document.getElementById('menu-button').src = "./assets/menu-buguer-open.svg"
+    document.getElementById('menu-button').removeEventListener('click',closeMenu)
     
 }
 
-function closeMenu(){
-    
-    const element = document.getElementsByClassName('links')
-    element[0].style.display = 'none'
-    document.getElementById('menu-button').src = "./assets/menu-buguer-open.svg"
-    document.getElementById('menu-button').setAttribute("onclick","openMenu()");
+function toggleMenu(){
+    const links = document.getElementById('links')
+    links.classList.toggle('active')
+
+    document.getElementById('menu-button').src = "./assets/menu-buguer-close.svg"
+    const btnMobile = document.getElementById('menu-button')
+    btnMobile.addEventListener('click',closeMenu)
+
+    document.querySelector('.text').classList.toggle('deactive')
+    document.querySelector('.button-center').classList.toggle('deactive')
+    document.getElementById('title').classList.toggle('deactive')
+    document.querySelector('.greatcoffee').classList.toggle('deactive')
     
 }
+
+const btnMobile = document.getElementById('menu-button')
+btnMobile.addEventListener('click',toggleMenu)
+
+
+
 
